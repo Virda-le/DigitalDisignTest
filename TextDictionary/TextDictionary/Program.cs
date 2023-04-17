7bd18bc870c3b73f04940aabfb97f76599140293
Virda-le <80399960+Virda-le@users.noreply.github.com>
2023-04-17 13:06:39 +0300

    Add files via upload

---
+++ TextDictionary/TextDictionary/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using TextDictionary;
+
+Console.WriteLine("Enter path:");
+string? path = Utilities.EnterPath();
+await Utilities.CreateAndSaveFile(path);
+Console.WriteLine($"Done!Check your dictionary here {Utilities.GetNewPath(path)} \nPress any key to exit.");
+Console.ReadKey();
