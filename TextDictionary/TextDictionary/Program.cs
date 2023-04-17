9209712ad2c741acaed06a4b40651edf344a7350
Virda-le <80399960+Virda-le@users.noreply.github.com>
2023-04-17 13:05:18 +0300

    Delete TextDictionary directory

--- TextDictionary/TextDictionary/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿
-using TextDictionary;
-
-Console.WriteLine("Enter path:");
-string? path = Utilities.EnterPath();
-await Utilities.CreateAndSaveFile(path);
-Console.WriteLine($"Done!Check your dictionary here {Utilities.GetNewPath(path)} \nPress any key to exit.");
-Console.ReadKey();
